81cd6f3741e4bad255e8764e9abcbbc08a206ec6
6godpro <erickson5115@gmail.com>
2023-04-22 17:05:23 +0100

    initial upload task 100

---
+++ 100-shell_sort.c
@@ -0,0 +1,54 @@
+#include "sort.h"
+#include <stdbool.h>
+#include <stdio.h>
+
+
+/**
+ * swap_values - Swaps two values.
+ * @x: The first value.
+ * @y: The second value.
+ */
+void swap_values(int *x, int *y)
+{
+	int tmp;
+
+	tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+
+
+/**
+ * shell_sort - Sort a dataset using the Shell sort algorithm.
+ * @array: The array to be sorted.
+ * @size: The size of the array to be sorted.
+ */
+void shell_sort(int *array, size_t size)
+{
+	int i, j = 0, n = size, gap;
+	bool swap;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (gap = 1; gap < n; gap = gap * 3 + 1)
+		;
+
+	for (; gap >= 1; gap /= 3)
+	{
+		swap = false;
+		for (j = gap; j < n; j++)
+		{
+			for (i = j - gap; i >= 0; i -= gap)
+			{
+				if (array[i] < array[i + gap])
+					break;
+				swap_values(&array[i], &array[i + gap]);
+				swap = true;
+			}
+		}
+		if (swap == true)
+			print_array(array, size);
+
+	}
+}
